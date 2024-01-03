ae42a81e96f6e76ca60a3d5e1cd1ddb91d8a9402
Marcelino Saquende <102360616+msaquende18@users.noreply.github.com>
2024-01-03 23:30:07 +0100

    Update [slug].js

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -47,7 +47,7 @@ const productDetails = ({ product, products }) => {
                     (10)
                 </p>
               </div>
-              <h4>Details: </h4>
+              <h4>Detalhes: </h4>
               <p>{details}</p>
               <p className='price'>${price}</p>
               <div className="quantity">
@@ -64,17 +64,17 @@ const productDetails = ({ product, products }) => {
               </div>
               <div className="buttons">
                  <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>
-                    Add to Cart
+                    Adicionar ao Carrinho 
                  </button>
                  <button type="button" className="buy-now" onClick={handleBuyNow}>
-                    Buy Now
+                    Comprar
                  </button>
               </div>
           </div>
         </div>
 
         <div className="maylike-products-wrapper">
-        <h2>You may also like</h2>
+        <h2>Poderás também gostar de:</h2>
         <div className='marquee'>
           <div className='maylike-products-container track'>
                {products.map((item) => (
