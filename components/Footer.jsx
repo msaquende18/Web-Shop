6c9f65c82fb05c10a362f15cc40078b8621c6d9c
Marcelino Saquende <102360616+msaquende18@users.noreply.github.com>
2024-01-04 23:17:17 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className='footer-container'>
-    <p>2022 Lavousier Commercial Headphones All rights reserved</p>
+    <p>2022 Lavousier Commercial All rights reserved</p>
     <p className='icons'>
        <AiFillInstagram />
        <AiOutlineTwitter />
